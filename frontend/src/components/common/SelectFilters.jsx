59fdd4f53e5f2fb70f116b4f9d38eff5a84954df
Nikhil Dhumal <nikhildhumal2003@gmail.com>
2025-10-20 07:14:30 +0530

    code rafctor

--- frontend/src/components/common/SelectFilters.jsx
+++ frontend/src/components/common/SelectFilters.jsx
@@ -1,11 +1,45 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Select from "react-select";
 
 import districtApi from "../../api/modules/districtsApi.js";
 import hospitalsApi from "../../api/modules/hospitalsApi.js";
 
 import { setDistricts } from "../../redux/features/districtsSlice.js";
 import { setHospitals } from "../../redux/features/hospitalsSlice.js";
+
+const selectStyles = {
+  control: (base, state) => ({
+    ...base,
+    border: "none",
+    borderBottom: state.isDisabled ? "none" : "2px solid #1565C0",
+    borderRadius: 0,
+    boxShadow: "none",
+    zIndex: 10000,
+    backgroundColor: state.isDisabled ? "#eeeeee" : "#ffffff",
+    "&:hover": {
+      borderBottom: state.isDisabled ? "none" : "2px solid #1565C0",
+    },
+  }),
+  menu: (base) => ({
+    ...base,
+    zIndex: 9999,
+  }),
+  dropdownIndicator: (base, state) => ({
+    ...base,
+    color: state.isDisabled ? "#bdbdbd" : "#1565C0",
+    "&:hover": {
+      color: state.isDisabled ? "#bdbdbd" : "#1565C0",
+    },
+  }),
+  clearIndicator: (base, state) => ({
+    ...base,
+    color: state.isDisabled ? "#bdbdbd" : "#E53935",
+    "&:hover": {
+      color: state.isDisabled ? "#bdbdbd" : "#E53935",
+    },
+  }),
+};
 
 const SelectFilters = ({ selected, setSelected }) => {
   const dispatch = useDispatch();
@@ -14,89 +48,97 @@ const SelectFilters = ({ selected, setSelected }) => {
   const { districts } = useSelector((state) => state.districts);
   const { hospitals } = useSelector((state) => state.hospitals);
 
-  const handleStateChange = async (e) => {
-    (async () => {
-      const { res, err } = await districtApi.getDistricts({
-        stateId: e.target.value,
-      });
-      if (res) dispatch(setDistricts(res));
-      if (err) console.log(err);
-    })();
-    (async () => {
-      const { res, err } = await hospitalsApi.getHospitals({
-        stateId: e.target.value,
-      });
-      if (res) dispatch(setHospitals(res));
-      if (err) console.log(err);
-    })();
-    setSelected({ state: e.target.value, district: "", hospital: "" });
+  const handleStateChange = async (option) => {
+    const stateId = option?.value;
+
+    const { res: districtRes, err: districtErr } =
+      await districtApi.getDistricts({ stateId });
+    if (districtRes) dispatch(setDistricts(districtRes));
+    if (districtErr) console.error(districtErr);
+
+    const { res: hospitalRes, err: hospitalErr } =
+      await hospitalsApi.getHospitals({ stateId });
+    if (hospitalRes) dispatch(setHospitals(hospitalRes));
+    if (hospitalErr) console.error(hospitalErr);
+
+    setSelected({
+      state: option,
+      district: null,
+      hospital: null,
+    });
   };
 
-  const handleDistrictChange = async (e) => {
-    (async () => {
-      const { res, err } = await hospitalsApi.getHospitals({
-        stateId: selected.state,
-        districtId: e.target.value,
-      });
-      if (res) dispatch(setHospitals(res));
-      if (err) console.log(err);
-    })();
+  const handleDistrictChange = async (option) => {
+    const districtId = option?.value;
+    const stateId = selected.state.value;
+
+    const { res, err } = await hospitalsApi.getHospitals({
+      stateId,
+      districtId,
+    });
+    if (res) dispatch(setHospitals(res));
+    if (err) console.error(err);
+
     setSelected((prev) => ({
-      state: prev.state,
-      district: e.target.value,
-      hospital: "",
+      ...prev,
+      district: option,
+      hospital: null,
     }));
   };
 
-  const handleHospitalChange = async (e) => {
-    (async () => {
-      const { res, err } = await hospitalsApi.getHospitals({
-        stateId: selected.state,
-        districtId: selected.district,
-        hospitalId: e.target.value,
-      });
-      if (res) dispatch(setHospitals(res));
-      if (err) console.log(err);
-    })();
-    setSelected((prev) => ({ ...prev, hospital: e.target.value }));
+  const handleHospitalChange = (option) => {
+    setSelected((prev) => ({
+      ...prev,
+      hospital: option,
+    }));
   };
 
   return (
     <div className="select-filters">
-      <select value={selected.state} onChange={handleStateChange}>
-        <option value="">Select State</option>
-        {states.map((state) => (
-          <option key={state.state_id} value={state.state_id}>
-            {state.state_name}
-          </option>
-        ))}
-      </select>
-
-      <select
+      <Select
+        className="filter"
+        styles={selectStyles}
+        value={selected.state}
+        options={states.map((s) => ({
+          value: s.state_id,
+          label: s.state_name,
+          latitude: s.latitude,
+          longitude: s.longitude,
+        }))}
+        onChange={handleStateChange}
+        placeholder="Select State"
+        isClearable
+      />
+      <Select
+        className="filter"
+        styles={selectStyles}
         value={selected.district}
+        options={districts.map((d) => ({
+          value: d.district_id,
+          label: d.district_name,
+          latitude: d.latitude,
+          longitude: d.longitude,
+        }))}
         onChange={handleDistrictChange}
-        disabled={!selected.state}
-      >
-        <option value="">Select District</option>
-        {districts.map((district) => (
-          <option key={district.district_id} value={district.district_id}>
-            {district.district_name}
-          </option>
-        ))}
-      </select>
-
-      <select
+        placeholder="Select District"
+        isDisabled={!selected.state?.value}
+        isClearable
+      />
+      <Select
+        className="filter"
+        styles={selectStyles}
         value={selected.hospital}
+        options={hospitals.map((h) => ({
+          value: h.hospital_id,
+          label: h.hospital_name,
+          latitude: h.latitude,
+          longitude: h.longitude,
+        }))}
         onChange={handleHospitalChange}
-        disabled={!selected.district}
-      >
-        <option value="">Select Hospital</option>
-        {hospitals.map((hospital) => (
-          <option key={hospital.hospital_id} value={hospital.hospital_id}>
-            {hospital.hospital_name}
-          </option>
-        ))}
-      </select>
+        placeholder="Select Hospital"
+        isDisabled={!selected.district?.value}
+        isClearable
+      />
     </div>
   );
 };
