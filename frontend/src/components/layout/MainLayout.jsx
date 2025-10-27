a4d5c08eec78316bdbb36a381d288c7b52f3526f
Nikhil Dhumal <nikhildhumal2003@gmail.com>
2025-10-27 10:11:20 +0530

    code refactor

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -3,13 +3,11 @@ import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 
 import statesApi from "../../api/modules/statesApi.js";
-import hospitalsApi from "../../api/modules/hospitalsApi.js";
 
 import NavBar from "../common/Navbar";
 import Footer from "../common/Footer";
 
 import { setStates } from "../../redux/features/statesSlice.js";
-import { setHospitals } from "../../redux/features/hospitalsSlice.js";
 
 const MainLayout = () => {
   const dispatch = useDispatch();
@@ -21,14 +19,7 @@ const MainLayout = () => {
     else if (err) console.log(err);
   };
 
-  const fetchHospitals = async () => {
-    const { res, err } = await hospitalsApi.getHospitals();
-    if (res) dispatch(setHospitals(res));
-    else dispatch(setHospitals([]));
-  };
-
   fetchStates();
-  fetchHospitals();
 }, [dispatch]);
 
   return (
